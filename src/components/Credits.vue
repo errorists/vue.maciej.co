c824b1723284e64781e0e573216b5ad128987fbe
Maciej Zadykowicz <hi@maciej.co>
2021-05-11 22:12:39 +0200

    Update Credits.vue

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -3,7 +3,7 @@
     <h3>about</h3>
     <div>
       <section>
-        <p>My name is Maciej, pronounced ‘much-yay’. I’m a designer, engineer, friend and musician making things on the Internet.</p>
+        <p>My name is Maciej, pronounced ‘much-yay’. I’m a designer, engineer, friend and musician making things on the Internet. He/Him</p>
         <figure><img src='../assets/p1.jpg' alt="Me" width="975" height="750"/></figure>
       </section>
       <section>
